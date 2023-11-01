2d02953dd41f5bc605d2310069e07817e1f5beea
Gordon Tran <itsnotgordon@gmail.com>
2023-11-01 14:53:24 -0700

    Used chrome storage sync to save todo list whenever you close the extension

--- src/script.js
+++ src/script.js
@@ -1,16 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const addTaskButton = document.getElementById('addTaskButton');
-    const taskInput = document.getElementById('taskInput');
-    const taskList = document.getElementById('taskList');
+
+    const addTaskButton = document.getElementById('addTaskButton')
+    const taskInput = document.getElementById('taskInput')
+    const taskList = document.getElementById('taskList')
+
+    //create an item
+    //have it appear under taskList
 
     addTaskButton.addEventListener('click', function() {
-        const taskText = taskInput.value.trim();
+        const taskText = taskInput.value.trim(); //trims spaces at beginning and end of string
 
-        if (taskText !== '') {
+        if (taskText !== '') { //if they actually entered text
+            const taskItem = document.createElement('li')
+            taskItem.textContent = taskText
+            //taskItem = HTML object
+            //taskText = string
+            //textContent access's taskItem's string portion
+            taskList.appendChild(taskItem)
+            taskInput.value = '' //reset the input
+
+            chrome.storage.sync.get({tasks : []}, function(data){
+                let storedTasks = data.tasks
+                storedTasks.push(taskText)
+                chrome.storage.sync.set({tasks: storedTasks})
+            })
+
+        }
+    })
+
+    chrome.storage.sync.get({ tasks: [] }, function(data) {
+        const storedTasks = data.tasks;
+        storedTasks.forEach(function(taskText) {
             const taskItem = document.createElement('li');
             taskItem.textContent = taskText;
             taskList.appendChild(taskItem);
-            taskInput.value = ''; // Clear the input after adding the task
-        }
+        });
     });
-});
+})
